ffc61581837cefce02713f1f5bf57843f20c71bd
JP <jp.bonner@bonner.jp>
2021-01-31 00:46:38 -0700

    respond to ecs healthchecks

--- lib/short_stuff_web/endpoint.ex
+++ lib/short_stuff_web/endpoint.ex
@@ -1,6 +1,8 @@
 defmodule ShortStuffWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :short_stuff
 
+  plug HealthCheck
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
@@ -15,6 +17,13 @@ defmodule ShortStuffWeb.Endpoint do
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+
+  # Admin interface
+  plug Plug.Static,
+    at: "/kaffy",
+    from: :kaffy,
+    gzip: false,
+    only: ~w(assets)
 
   # Serve at "/" the static files from "priv/static" directory.
   #
