e6af00e7446c8bb97e7402822a608e769000814c
JP <jp.bonner@bonner.jp>
2021-01-31 17:30:23 -0700

    disable debug

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -14,7 +14,7 @@ data "template_file" "task_template_secretsmanager" {
     db_password_secret     = aws_secretsmanager_secret.database_password_secret.arn
     secret_key_base_secret = aws_secretsmanager_secret.secret_key_base.arn
     signing_salt_secret    = aws_secretsmanager_secret.signing_salt.arn
-    admin_password_secret  = aws_secretsmanager_secret.signing_salt.arn
+    admin_password_secret  = aws_secretsmanager_secret.admin_password.arn
   }
 }
 
@@ -27,7 +27,12 @@ data "template_file" "task_template_secretsmanager" {
 data "aws_iam_policy_document" "password_policy_secretsmanager" {
   statement {
     actions   = ["secretsmanager:GetSecretValue"]
-    resources = [aws_secretsmanager_secret.database_password_secret.arn]
+    resources = [
+      aws_secretsmanager_secret.database_password_secret.arn,
+      aws_secretsmanager_secret.secret_key_base.arn,
+      aws_secretsmanager_secret.signing_salt.arn,
+      aws_secretsmanager_secret.admin_password.arn,
+    ]
   }
 }
 
@@ -51,12 +56,21 @@ resource "aws_secretsmanager_secret_version" "secret_key_base_version" {
 }
 
 resource "aws_secretsmanager_secret" "signing_salt" {
-  name = "${var.environment_name}-${var.name}-signing-sale"
+  name = "${var.environment_name}-${var.name}-signing-salt"
 }
 
 resource "aws_secretsmanager_secret_version" "signing_salt_version" {
   secret_id     = aws_secretsmanager_secret.signing_salt.id
   secret_string = var.signing_salt
+}
+
+resource "aws_secretsmanager_secret" "admin_password" {
+  name = "${var.environment_name}-${var.name}-admin-password"
+}
+
+resource "aws_secretsmanager_secret_version" "admin_password_version" {
+  secret_id     = aws_secretsmanager_secret.admin_password.id
+  secret_string = var.admin_password
 }
 
 resource "aws_iam_policy" "password_policy_secretsmanager" {
