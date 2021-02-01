b1c64dbc903d494c56469f5dc61b3bac47bbe1f5
JP <jp.bonner@bonner.jp>
2021-02-01 16:14:55 -0700

    fix image tags

--- lib/short_stuff_web/views/layout_view.ex
+++ lib/short_stuff_web/views/layout_view.ex
@@ -5,6 +5,6 @@ defmodule ShortStuffWeb.LayoutView do
   As in "Is the squeeze squozen yet?"
   """
   def squeeze_status_answer() do
-    if ShortStuff.Shorts.Squeeze.are_squeezed?(), do: "yes!", else: "no."
+    if ShortStuff.Shorts.Squeeze.are_squeezed?(), do: "yes", else: "no"
   end
 end
