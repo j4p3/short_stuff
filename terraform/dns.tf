84156614a146c7e4ab810b2789dde1fc2dc32f97
JP <jp.bonner@bonner.jp>
2021-02-01 02:49:52 -0700

    update tf dns

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -8,13 +8,13 @@ resource "aws_route53_zone" "main" {
   }
 }
 
-resource "aws_route53_zone" "dev" {
-  name = "dev.isthesqueezesquoze.com"
+# resource "aws_route53_zone" "dev" {
+#   name = "dev.isthesqueezesquoze.com"
 
-  tags = {
-    environment = "dev"
-  }
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
 
 # Existing records
 
@@ -24,6 +24,7 @@ resource "aws_route53_zone" "dev" {
 #   type            = "A"
 #   allow_overwrite = true
 # }
+
 resource "aws_route53_record" "ns" {
   zone_id         = aws_route53_zone.main.id
   name            = "isthesqueezesquoze.com."
@@ -37,6 +38,7 @@ resource "aws_route53_record" "ns" {
     "1793.awsdns-32.co.uk."
   ]
 }
+
 resource "aws_route53_record" "soa" {
   zone_id         = aws_route53_zone.main.id
   name            = "isthesqueezesquoze.com."
@@ -45,6 +47,7 @@ resource "aws_route53_record" "soa" {
   ttl             = "900"
   records         = ["ns-324.awsdns-40.com. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
 }
+
 resource "aws_route53_record" "cname_cloudfront_a" {
   zone_id         = aws_route53_zone.main.id
   name            = "_ddcf9ec0f380a1f597ffa5f0ce484549.isthesqueezesquoze.com."
@@ -53,6 +56,7 @@ resource "aws_route53_record" "cname_cloudfront_a" {
   ttl             = "300"
   records         = ["_de1255e84b188dd1ae4fb165d90ce0fc.vtqfhvjlcp.acm-validations.aws."]
 }
+
 resource "aws_route53_record" "cname_cloudfront_b" {
   zone_id         = aws_route53_zone.main.id
   name            = "_8a0ac2de3a061f3694ef01a689d19676.www.isthesqueezesquoze.com."
@@ -62,14 +66,14 @@ resource "aws_route53_record" "cname_cloudfront_b" {
   records         = ["_da3d8999beb0e3289cc0c504cad7ed3d.vtqfhvjlcp.acm-validations.aws."]
 }
 
-resource "aws_route53_record" "ns_dev" {
-  zone_id         = aws_route53_zone.main.zone_id
-  name            = "dev.example.com"
-  type            = "NS"
-  ttl             = "30"
-  records         = aws_route53_zone.dev.name_servers
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "ns_dev" {
+#   zone_id         = aws_route53_zone.main.zone_id
+#   name            = "dev.isthesqueezesquoze.com"
+#   type            = "NS"
+#   ttl             = "30"
+#   records         = aws_route53_zone.dev.name_servers
+#   allow_overwrite = true
+# }
 
 resource "aws_route53_record" "cname_dev" {
   zone_id = aws_route53_zone.main.zone_id
